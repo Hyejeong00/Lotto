73a044c765ede06821765587f5bad93795b9e11e
Hyejeong00 <love72771112@gmail.com>
2021-05-26 15:07:26 +0900

    Shuffling

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -31,6 +31,16 @@ fun getRandomLottoNumbers(): MutableList<Int>{
             }
             return lottoNumbers
         }
+fun getShuffledLottoNumbers () : MutableList<Int>{
+    val list = mutableListOf<Int>()
+
+    for (number in 1..45){
+        list.add(number)
+    }
+    list.shuffle()
+
+    return list.subList(0, 6)
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +61,8 @@ class MainActivity : AppCompatActivity() {
 
         btnRandom.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
-            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            //intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            intent.putIntegerArrayListExtra("result", ArrayList(getShuffledLottoNumbers()))
             startActivity(intent)
         }
     }
